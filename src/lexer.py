05303c28ac07c70fe7fd2ee0dee695725357cb13
starloexoliz11 <mgamerdinge146@gmail.com>
2026-03-30 10:38:36 +0800

    Update lexer.py

--- src/lexer.py
+++ src/lexer.py
@@ -3,6 +3,9 @@ import re
 
 T_REGEX = r'(@\w+)|("(?:[^"]*)")|(\d+)|([=:+\-*/><!]+)|(\w+)'
 
+def get_indent_lvl(line):
+    return len(line) - len(line.lstrip(" "))
+
 def tokenize(line):
     tokens = []
     for match in re.finditer(T_REGEX, line):
