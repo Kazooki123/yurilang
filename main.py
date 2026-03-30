32e23145cf5767f28b9560fe595de9e49ebb736f
starloexoliz11 <mgamerdinge146@gmail.com>
2026-03-30 14:54:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ if __name__ == "__main__":
         print("Usage: python main.py <file.yuri>")
         exit()
 
+    # Testing
+    with open("test.yuri", "r") as f:
+        code = f.read()
+
     with open(sys.argv[1], "r") as f:
         code = f.read()
 
