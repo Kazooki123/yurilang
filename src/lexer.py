9425916042af1299b9edfbf68e33d0f741a351e4
starloexoliz11 <mgamerdinge146@gmail.com>
2026-04-02 12:40:02 +0800

    Update lexer.py

--- src/lexer.py
+++ src/lexer.py
@@ -1,6 +1,7 @@
 # YuriLangs main lexer
 import re
 
+
 T_REGEX = r'(#\[\[[^\]]*\]\])|(\[\[[^\]]*\]\])|(@\w+)|("(?:[^"]*)")|(\d+)|([=:+\-*/><!]+)|(\w+\.\w+)|(\w+)'
 
 
@@ -8,6 +9,13 @@ def get_indent_lvl(line):
     return len(line) - len(line.lstrip(" "))
 
 def tokenize(line):
+    comment_match = re.search(r'\?\s*\(.*\)', line)
+    if comment_match:
+        line = line[:comment_match.start()]
+
+    if not line.strip():
+        return []
+
     tokens = []
     for match in re.finditer(T_REGEX, line):
         tokens.append(match.group())
