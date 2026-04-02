c924f465d28253b19e17d648ab219ccd61e3cbe8
starloexoliz11 <mgamerdinge146@gmail.com>
2026-04-02 20:17:51 +0800

    Update repl.py

--- src/repl.py
+++ src/repl.py
@@ -30,7 +30,7 @@ def shoutouts():
 
     names = [
         "@jamiw (1024ping)", "@gseppo", "@lunalapigeonne",
-        "@anormalwintrovert", "@hexagonos", "@theophilus_dev", "@iiiangel", "@asciixd", "@themackabu", "@solaenum (luci)", "@vt_d (vitam1n)"
+        "@anormalwintrovert", "@hexagonos", "@theophilus_dev", "@iiiangel", "@asciixd", "@themackabu", "@solaenum (luci)", "@vt_d (vitam1n)", "@aleks_minecraft1"
     ]
 
     for i in range(0, len(names), 2):
