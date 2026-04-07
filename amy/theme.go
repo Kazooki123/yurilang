25bbd4eac8f71f0c9d7a7f42da3dcbc251df3959
Kazooki123 <mgamerdinge146@gmail.com>
2026-04-08 06:48:40 +0800

    Added the amy editor

---
+++ amy/theme.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gdamore/tcell/v2"
+)
+
+type Theme struct {
+	Background tcell.Color
+	Foreground tcell.Color
+	Accent     tcell.Color
+	StatusBg   tcell.Color
+	StatusFg   tcell.Color
+}
+
+var LesbianTheme = Theme{
+	Background: tcell.ColorBlack,
+	Foreground: tcell.ColorWhite,
+	Accent:     tcell.ColorHotPink,
+	StatusBg:   tcell.ColorDarkOrange,
+	StatusFg:   tcell.ColorWhite,
+}
+
