ecba9c031b99d2089021636fe358495cf0dee28d
Kazooki123 <mgamerdinge146@gmail.com>
2026-04-10 07:17:22 +0800

    small changes/adjustments on the core files

--- src/repl.py
+++ src/repl.py
@@ -113,7 +113,8 @@ def show_credits():
     print("Contributors:")
     names = [
         "@themackabu    → JS-Port",
-        "@@douxx.tech   → Added C callers"
+        "@@douxx.tech   → Added C callers",
+        "@iiiangel      → Helped the Amy Editor"
     ]
 
     for i, name in enumerate(names):
